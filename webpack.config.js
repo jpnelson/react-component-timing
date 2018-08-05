c4b33d998a71b5fd63337c7120d829d9c4a3086d
Joshua Nelson <jonelson@atlassian.com>
2018-08-05 14:29:48 -0700

    patch release

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,19 @@ module.exports = {
     ]
   },
   externals: {
-    react: "React",
-    "react-dom": "ReactDOM"
+    react: {
+      root: "React",
+      commonjs2: "react",
+      commonjs: "react",
+      amd: "react",
+      umd: "react"
+    },
+    "react-dom": {
+      root: "ReactDOM",
+      commonjs2: "react-dom",
+      commonjs: "react-dom",
+      amd: "react-dom",
+      umd: "react-dom"
+    }
   }
 };
